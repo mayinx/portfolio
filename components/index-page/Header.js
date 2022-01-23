d7627a6bb08a862460fc267703c66612e23af3a5
mayinx <mayinxx@web.de>
2022-01-23 21:04:00 +0100

    Beginning transition to sass/scss...

--- components/index-page/Header.js
+++ components/index-page/Header.js
@@ -5,7 +5,7 @@ import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 
 // import "./Header.module.css";
-import styles from "./Header.module.css";
+import styles from "./Header.module.scss";
 
 export default function Header() {
   // FYI: Necessary check to ensure that the window object (and thus 'document') is loaded and available
@@ -29,19 +29,27 @@ export default function Header() {
           {/* <Navbar.Brand href="/">Christian Daum | Full Stack Developer</Navbar.Brand> */}
           <Navbar.Brand
             href="/"
-            className="d-flex flex-column align-items-start  justify-content-center flex-sm-row gap-sm-2 align-items-sm-center"
+            className={`${styles.navBrand} d-flex flex-column align-items-start  justify-content-center flex-sm-row gap-sm-2 align-items-sm-center`}
           >
-            <span>Christian Daum</span>
+            <span className={styles.navBrandHead}>Christian Daum</span>
             <span className="d-none d-sm-block text-white-50"> | </span>
             <span className={`${styles.navBrandSubhead} text-white-50`}>Full Stack Developer</span>
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Toggle class={styles.navToggler} aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/">Start</Nav.Link>
-              <Nav.Link href="#featured">Projects</Nav.Link>
-              <Nav.Link href="#about">About</Nav.Link>
-              <Nav.Link href="#skills">Skills & Tools</Nav.Link>
+              <Nav.Link class={styles.navLink} href="/">
+                Start
+              </Nav.Link>
+              <Nav.Link class={styles.navLink} href="#featured">
+                Projects
+              </Nav.Link>
+              <Nav.Link class={styles.navLink} href="#about">
+                About
+              </Nav.Link>
+              <Nav.Link class={styles.navLink} href="#skills">
+                Skills & Tools
+              </Nav.Link>
               {/* <Nav.Link href="#upcoming">Upcoming</Nav.Link> */}
               {/* <Nav.Link href="#contact">Blog</Nav.Link> */}
             </Nav>

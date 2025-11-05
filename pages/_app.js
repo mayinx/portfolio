2674ae205bd7e1c710b94caf2593cb0d7860f1fb
mayinx <mayinxx@web.de>
2025-11-05 15:59:06 +0100

    chore: add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 // FYI: Component prop == the currently active page
 function PortfolioApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default PortfolioApp;
